73c90a601db5ed3ca72ef529509a46e6a7610c95
Knolle-03 <willem-lennart.draeger@haw-hamburg.de>
2020-05-08 18:54:59 +0200

    housekeeping

--- src/de/hawh/ld/GKA01/main/Main.java
+++ src/de/hawh/ld/GKA01/main/Main.java
@@ -1,43 +1,41 @@
 package de.hawh.ld.GKA01.main;
 
 import de.hawh.ld.GKA01.algorithms.eulerian.circuit.Hierholzer;
-import de.hawh.ld.GKA01.conversion.ListFromGraph;
-import de.hawh.ld.GKA01.io.FileWriter;
 import de.hawh.ld.GKA01.util.ColoringType;
 import de.hawh.ld.GKA01.util.Painter;
-import de.hawh.ld.GKA01.util.generators.RandomEulerianGenerator;
+import org.graphstream.algorithm.generator.DorogovtsevMendesGenerator;
 import org.graphstream.algorithm.generator.Generator;
 import org.graphstream.graph.Graph;
 import org.graphstream.graph.implementations.SingleGraph;
 
-import java.util.List;
 
 
 public class Main {
 
     public static void main(String[] args) {
 
-        for (int i = 0; i < 13 ; i++) {
-            Generator generator = new RandomEulerianGenerator();
+        for (int i = 0; i < 1 ; i++) {
+            Generator generator = new DorogovtsevMendesGenerator();
             Graph graph = new SingleGraph("nonEulerianGraph(" + (i + 3) + " nodes)" , false, true);
             generator.addSink(graph);
             generator.begin();
             generator.end();
+            graph.display();
+
 
             Hierholzer hierholzer = new Hierholzer();
             hierholzer.init(graph);
             hierholzer.compute();
 
-            graph.display();
 
             Painter painter = new Painter();
             painter.attach(graph, ColoringType.MINIMAL_SPANNING_TREE);
             painter.colorGraph(hierholzer.getEulerianTour());
 
 
-            String path = "Test\\testResources\\smallNonEulerianTestGraphs\\" + graph.getId() + ".graph";
-            List<String> lines = ListFromGraph.getFileLines(graph);
-            FileWriter.writeLines(lines, path);
+//            String path = "Test\\testResources\\smallNonEulerianTestGraphs\\" + graph.getId() + ".graph";
+//            List<String> lines = ListFromGraph.getFileLines(graph);
+//            FileWriter.writeLines(lines, path);
 
         }
 
